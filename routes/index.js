2b945ec586080d9c503f938afc38630125fb79d1
Izabella Chebakova <chebakovaizabella@gmail.com>
2023-01-11 20:50:23 +0400

    Добавление контроллеров для movies

--- routes/index.js
+++ routes/index.js
@@ -2,16 +2,9 @@ const router = require('express').Router();
 const { celebrate, Joi } = require('celebrate');
 const auth = require('../middlewares/auth');
 const user = require('./users');
-// const movie = require('./movies');
+const movie = require('./movies');
 const { login, createUser } = require('../controllers/users');
 const PageNotFoundError = require('../errors/pageNotFoundError');
-
-// для краш-теста
-// router.get('/crash-test', () => {
-//   setTimeout(() => {
-//     throw new Error('Сервер сейчас упадёт');
-//   }, 0);
-// });
 
 router.post('/signin', celebrate({
   body: Joi.object().keys({
@@ -19,13 +12,6 @@ router.post('/signin', celebrate({
     password: Joi.string().required(),
   }),
 }), login);
-
-// для краш-теста
-// router.get('/crash-test', () => {
-//   setTimeout(() => {
-//     throw new Error('Сервер сейчас упадёт');
-//   }, 0);
-// });
 
 router.post('/signup', celebrate({
   body: Joi.object().keys({
@@ -38,7 +24,7 @@ router.post('/signup', celebrate({
 router.use(auth);
 
 router.use('/users', user);
-// router.use('/movies', movie);
+router.use('/movies', movie);
 
 router.use('/', (req, res, next) => {
   next(new PageNotFoundError());
