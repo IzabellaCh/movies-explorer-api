d1cd0ee853b875357acfc25449f629ad9cd4d40e
Izabella Chebakova <chebakovaizabella@gmail.com>
2023-01-12 17:50:11 +0400

    Добавление cors, добавление домена фронта

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const express = require('express');
-// const cors = require('cors');
-// const rateLimit = require('express-rate-limit');
+const cors = require('cors');
 const helmet = require('helmet');
 const cookieParser = require('cookie-parser');
 const mongoose = require('mongoose');
@@ -14,23 +13,23 @@ const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 const app = express();
 
-// const options = {
-//   origin: [
-//     'http://localhost:3000',
-//     'https://localhost:3000',
-//     'http://mesto.edu-project.nomoredomains.club',
-//     'https://mesto.edu-project.nomoredomains.club',
-//   ],
-//   methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
-//   preflightContinue: false,
-//   optionsSuccessStatus: 204,
-//   allowedHeaders: ['Content-Type', 'origin', 'Authorization'],
-//   credentials: true,
-// };
+const options = {
+  origin: [
+    'http://localhost:3000',
+    'https://localhost:3000',
+    'http://movie-project.nomoredomains.club',
+    'https://movie-project.nomoredomains.club',
+  ],
+  methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+  preflightContinue: false,
+  optionsSuccessStatus: 204,
+  allowedHeaders: ['Content-Type', 'origin', 'Authorization'],
+  credentials: true,
+};
 
-// app.use('*', cors(options));
+app.use('*', cors(options));
 
-// require('dotenv').config();
+require('dotenv').config();
 
 // ограничение количества запросов
 app.use(limiter);
@@ -44,6 +43,7 @@ app.use(cookieParser());
 mongoose.connect(mongoDB);
 
 app.use(requestLogger);
+
 app.use('/', router);
 
 app.use(errorLogger);
